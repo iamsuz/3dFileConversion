1ec1b111e5331857177e09f8b2fab947caf50558
Sujit Mahavarkar <s.mahavarkar@gmail.com>
2023-09-19 22:43:36 +0530

    updates the texture to extract with all the information

--- lib/extractTexture.py
+++ lib/extractTexture.py
@@ -1,65 +1,56 @@
 import os
 import bpy
 
-# Enable the glTF 2.0 addon
+# Enable the glTF 2.0 Addon
 bpy.ops.preferences.addon_enable(module='io_scene_gltf2')
 
-
-def extract_material_textures(material):
-    texture_images = []
-
-    for node in material.node_tree.nodes:
-        if node.type == 'TEX_IMAGE':
-            texture_images.append({
-                "image": node.image,
-                "name": node.label
-            })
-
-    return texture_images
+# Function to extract and save material textures
 
 
-def save_texture_images(material_textures, output_directory):
-    for material_name, texture_images in material_textures.items():
-        material_dir = os.path.join(output_directory, material_name)
-        os.makedirs(material_dir, exist_ok=True)
-        # print(texture_images)
-        for idx, texture_image in enumerate(texture_images):
-            print(texture_image['name'])
-            output_file = os.path.join(
-                material_dir, f'{texture_image["name"]}.png')
-            texture_image['image'].save_render(output_file)
+def extract_and_save_material_textures(material, output_directory):
+    # Check if the material has a node tree
+    if material.node_tree:
+        # Loop through the nodes in the shader node tree
+        for node in material.node_tree.nodes:
+            # Check if the node is an Image Texture node
+            if node.type == 'TEX_IMAGE':
+                # Get the texture image
+                texture_image = node.image
+                # Extract the texture type (e.g., Base, Diffuse, Roughness)
+                texture_type = node.label  # Use node.label if it contains the texture type
+                # Define the output file path
+                output_file = os.path.join(
+                    output_directory, f'{texture_type}.png')
+                # Save the texture image
+                texture_image.save_render(output_file)
+
+# Function to initialize the script
 
 
 def initialize(input_file, output_directory):
-    # Clear existing data to prevent conflicts
+    # Clear existing data
     bpy.ops.wm.read_factory_settings(use_empty=True)
 
     # Check if the input file exists
     if not os.path.exists(input_file):
-        raise FileNotFoundError(
-            "Input file does not exist: {}".format(input_file))
+        raise FileNotFoundError(f"Input file does not exist: {input_file}")
 
-    # Import the glTF file
+    # Import the glTF 2.0 model
     bpy.ops.import_scene.gltf(filepath=input_file, filter_glob='*.glb;*.gltf')
 
-    material_textures = {}
-
-    # Loop through selected objects
+    # Loop through selected objects (assumes objects are meshes)
     for obj in bpy.context.selected_objects:
-        if obj.type == 'MESH':
-            # Loop through material slots
-            for slot in obj.material_slots:
-                material = slot.material
-                if material:
-                    # Extract material textures
-                    texture_images = extract_material_textures(material)
-
-                    # Store material textures in a dictionary
-                    material_textures[material.name] = texture_images
-
-    # Save the texture images
-    save_texture_images(material_textures, output_directory)
+        # Loop through material slots
+        for slot in obj.material_slots:
+            # Get the material
+            material = slot.material
+            # Check if a material exists
+            if material:
+                # Extract and save material textures with their original names
+                extract_and_save_material_textures(material, output_directory)
 
 
 # Example usage
-initialize('toteBag.glb', 'output_textures')
+input_file = 'your_model.glb'
+output_directory = 'output_textures'
+initialize(input_file, output_directory)
